6674111a286271796428bc9a0183fc15d0879470
richarthurs <arthursrich@gmail.com>
2017-10-05 00:41:36 -0700

    Made install executable, added some sweet ASCII art and better comments

--- guidance_install.sh
+++ guidance_install.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
+# This script installs APM, DroneKit, and required dependencies.
+# Running this will get you the correct environment and configuration to work on the guidance script.
+# Author: Richard Arthurs
+# Written: Oct. 4, 2017
 
-echo "Cloning APM from git..."
-cd ~/
+echo "--------------------- Team Guardian Autopilot Setup Script!!! :D ---------------------"
+echo "Note: you will need to enter your password and 'y' several times."
+echo "Installation takes roughly 10 minutes."
+echo "Please modify this script if you don't want things in your root directory. (see comments)"
+echo ""
+
+echo "--------------------- Cloning APM from git ---------------------"
+echo ""
+cd ~/ #Modify for non-root install
 git clone git://github.com/ArduPilot/ardupilot.git
-cd ~/ardupilot
+cd ~/ardupilot #Modify for non-root install
 git submodule update --init --recursive
 
 sudo apt-get install python-matplotlib python-serial python-wxgtk3.0 python-wxtools python-lxml
@@ -16,16 +27,26 @@ export PATH=/usr/lib/ccache:$PATH
 
 . ~/.bashrc
 
-cd ~/
+cd ~/ #Modify for non-root install
 
-echo "Installing DroneKit..."
+echo "--------------------- Installing DroneKit ---------------------"
+echo ""
 sudo apt-get install python-pip python-dev
 pip install dronekit
 
-echo "Updating APM locations.txt to include Surrey field"
-cp ~/guardian_guidance/locations.txt ~/ardupilot/Tools/autotest
+echo "--------------------- Updating APM locations.txt to include Surrey field ---------------------" # we need this to avoid typing in coordinates every time, or flying from Australia to Surrey
+echo ""
+cp ~/guardian_guidance/locations.txt ~/ardupilot/Tools/autotest #Modify for non-root install
 
-echo "Installing other dependencies"
+echo "--------------------- Installing other dependencies ---------------------"
+echo ""
 sudo apt-get install xdotool
 
-echo "Good to go!"
+echo "--------------------- Good to go!---------------------"
+echo ""
+echo ""
+echo "/------------------------\              |~~\_____/~~\__  |
+|                        |______________ \______====== )-+
+|     TEAM GUARDIAN      |                      ~~~|/~~  |
+\------------------------/                         ()
+"
